cdfe8edb4da74e0f28e438ad31959ec9ef6419b7
Fedor Sharov <1710193@mail.ru>
2022-06-29 06:01:48 +0300

    reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double h, double m) {
-        double result = m / (h*h/10000);
+        double result = m / (h * h / 10000);
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         double h = 178;
         double m = 67.5;
-        double index = service.calculate(h,m);
+        double index = service.calculate(h, m);
         System.out.println("Ваш индекс тела: " + index);
     }
 }
